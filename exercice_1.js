f5ef38dca348fce4e666defdfd5bcecda92a58ab
Edwige08 <edwige.saves@gmail.com>
2025-01-29 16:07:01 +0100

    fin exo 1 et 2

--- exercice_1.js
+++ exercice_1.js
@@ -1,14 +1,14 @@
 // Nous souhaitons faire la somme du prix de plusieurs articles
 // Prix de trois articles
-let article1 = 10
-let article2 = 20
-let article3 = 50
+let article1 = 10;
+let article2 = 20;
+let article3 = 50;
 
 // Nous calculons le total
-let totalAvantReduc = article1 + article2 + article3
+let totalAvantReduc = article1 + article2 + article3;
 
 //Nous appliquons 20% de reduction
-let totalApresReduc = totalAvantReduc - totalAvantReduc * 0.2
+let totalApresReduc = totalAvantReduc - totalAvantReduc * 0.2;
 
-console.log("Le prix total avant reduction est de : ", totalAvantReduc)
-console.log("Le prix total après reduction est de : ", totalApresReduc)
+console.log("Le prix total avant reduction est de : ", totalAvantReduc);
+console.log("Le prix total après reduction est de : ", totalApresReduc);
--- exercice_2.js
+++ exercice_2.js
@@ -1,14 +1,14 @@
 //1ère partie de l'exercice :
 
-let prenom = "Edwige"
-let nom = "Saves"
+let prenom = "Edwige";
+let nom = "Saves";
 
-console.log('Bonjour, ' + nom + ' ' + prenom)
+console.log('Bonjour, ' + nom + ' ' + prenom);
 
 //2ème partie de l'exercice :
 
-const nombre = 64
-let age = 34
-let resultat = nombre - age
+const nombre = 64;
+let age = 34;
+let resultat = nombre - age;
 
-console.log(resultat + ' années à travailler avant la retraite ! Courage ! En développant ce sera plus fun ! ')
+console.log(resultat + ' années à travailler avant la retraite ! Courage ! En développant ce sera plus fun ! ');
