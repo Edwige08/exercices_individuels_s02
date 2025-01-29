dd51fca4ebbea901816a4a3a46a1c2be4f0a4b12
Edwige08 <edwige.saves@gmail.com>
2025-01-29 14:44:58 +0100

    first commit

---
+++ exercice_2.js
@@ -0,0 +1,14 @@
+//1ère partie de l'exercice :
+
+let prenom = "Edwige"
+let nom = "Saves"
+
+console.log('Bonjour, ' + nom + ' ' + prenom)
+
+//2ème partie de l'exercice :
+
+const nombre = 64
+let age = 34
+let resultat = nombre - age
+
+console.log(resultat + ' années à travailler avant la retraite ! Courage ! En développant ce sera plus fun ! ')
